8bc8295d652e8da1b1b234653806f161f16bca5f
Alvin Wang <alvinwang922@gmail.com>
2020-04-20 14:25:20 -0700

    Number of words in string problem

---
+++ Number-of-Words-in-String.py
@@ -0,0 +1,22 @@
+# Given a string, "Hello this is a string" write me a method to return the count of words in that string
+# Input: "Hello this is a string" -->  5
+# Caviotte, you cannot use any bulit methods such as split, and no built in functions
+# "         "  "  . . . 2 3 . / = +"
+
+
+def numberOfWords(words):
+    isSpace = True
+    counter = 0
+    for i in range(len(words)):
+        if words[i] != " " and isSpace == True:
+            counter += 1
+            isSpace = False
+        else:
+            isSpace = True
+    return counter
+
+
+print(numOfWords("This has  random   spaces"))
+print(numOfWords("  Spaces at front and end   "))
+print(numOfWords("        "))
+print("The values above should be 4, 5, and 0.")
