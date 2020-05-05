185e7fcd3242998adfdcd2e95f7df0e16fdef697
Alvin Wang <alvinwang922@gmail.com>
2020-05-04 17:16:04 -0700

    Group anagrams together

---
+++ Arrays/Group-Anagrams.py
@@ -0,0 +1,16 @@
+# Given an array of strings, group anagrams together.
+# Note: All inputs will be in lowercase.
+# The order of your output does not matter.
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[tuple(sorted(word))].append(word)
+        return anagrams.values()
+
+
+print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print("The return value above should be \
+[["ate","eat","tea"],["nat","tan"],["bat"]]")
