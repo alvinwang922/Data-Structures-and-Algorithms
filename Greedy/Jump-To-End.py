042b7b729bb901ff53a2ea192c23d4e633b0e2eb
Alvin Wang <alvinwang922@gmail.com>
2020-05-06 13:35:51 -0700

    Whether or not you can jump to end

--- Greedy/Jump-To-End.py
+++ Greedy/Jump-To-End.py
@@ -1,10 +1,9 @@
 # Given an array of non-negative integers,
 # you are initially positioned at the
 # first index of the array.
-
-Each element in the array represents your maximum jump length at that position.
-
-Determine if you are able to reach the last index.
+# Each element in the array represents your
+# maximum jump length at that position.
+# Determine if you are able to reach the last index.
 
 
 class Solution(object):
