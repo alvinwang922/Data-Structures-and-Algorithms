b873e0dde45705377a50b083e7e6610bc057e114
Alvin Wang <alvinwang922@gmail.com>
2020-05-30 18:54:27 -0700

    Determine winner in simple game

---
+++ Arrays/Simple-Game.py
@@ -0,0 +1,28 @@
+"""
+You are playing a game with one of your villagers. In the game, 
+there are two players where their respective scores are incremented 
+by one point at a time. Given an array of booleans, where True means 
+that Player 1 scores a point, and False means that Player 2 scores a 
+point. Write a program that outputs the winner of the game. If there 
+is no winner, return 0.
+"""
+
+
+def getWinner(gameScores):
+    counter = 0
+    for i in range(len(gameScores)):
+        if gameScores[i] is True:
+            counter += 1
+    if counter == len(gameScores) // 2 and len(gameScores) % 2 == 0:
+        return 0
+    elif counter > len(gameScores) // 2:
+        return 1
+    else:
+        return 2
+
+
+def main():
+    print(getWinner([true, false, true, false, false, false]))
+    print(getWinner([true, false, true, false, true, false, true]))
+    print(getWinner([true, false]))
+    print("The values above should be 2, 1, and 0.")
