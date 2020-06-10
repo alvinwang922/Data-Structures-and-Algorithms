62ee766b4ed53a2503860b91275608ef518dca2f
Alvin Wang <alvinwang922@gmail.com>
2020-06-10 16:47:49 -0700

    Generate all combinations for n set of parentheses using dynamic programming

--- DynamicProgramming/Generate-Parentheses.py
+++ DynamicProgramming/Generate-Parentheses.py
@@ -10,18 +10,13 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        result = []
-        self.backtrack(result, "", 0, 0, n * 2)
-        return result
-
-    def backtrack(self, result, current, opening, closing, length):
-        if len(current) == length:
-            result.append(current)
-            return result
-        if opening < length // 2:
-            self.backtrack(result, current + "(", opening+1, closing, length)
-        if closing < opening:
-            self.backtrack(result, current + ")", opening, closing + 1, length)
+        dp = [[] for i in range(n + 1)]
+        dp[0].append('')
+        for i in range(n + 1):
+            for j in range(i):
+                dp[i] += ['(' + x + ')' + y for x in dp[j]
+                          for y in dp[i - j - 1]]
+        return dp[n]
 
     def main():
         print(generateParenthesis(1))
