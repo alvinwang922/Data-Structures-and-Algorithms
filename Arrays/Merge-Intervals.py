1fd4fb933c48883f5897d3742d6f2dca46328130
Alvin Wang <alvinwang922@gmail.com>
2020-06-30 22:15:41 -0700

    Merge intervals with overlaps

---
+++ Arrays/Merge-Intervals.py
@@ -0,0 +1,21 @@
+"""
+Given a collection of intervals, merge all overlapping intervals.
+"""
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        final = []
+        for i in sorted(intervals):
+            if final and i[0] <= final[-1][1]:
+                final[-1][1] = max(final[-1][1], i[1])
+            else:
+                final += i,
+        return final
+
+
+print(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+print(merge([[1, 4], [4, 5]]))
+print(merge([]))
+print("The arrays above should be [[1, 6], [8, 10], [15, 18]], \
+    [[1, 5]], and [].")
