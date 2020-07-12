e6ebad046f6a7176138507407092336b6b08cae2
Alvin Wang <alvinwang922@gmail.com>
2020-07-12 14:35:50 -0700

    Return all subsets of a set of integers with duplicates

---
+++ Backtracking/Subsets-Two.py
@@ -0,0 +1,28 @@
+"""
+Given a collection of integers that might contain duplicates, nums, 
+return all possible subsets (the power set).
+Note: The solution set must not contain duplicate subsets.
+"""
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]):
+        res = []
+        nums.sort()
+        self.backtrack(nums, [], res)
+        return res
+
+    def backtrack(self, nums, subset, res):
+        res.append(subset)
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            self.backtrack(nums[i+1:], subset + [nums[i]], res)
+
+
+print(subsetsWithDup([1, 2, 2]))
+print(subsetsWithDup([1, 2, 4, 4]))
+print(subsetsWithDup([]))
+print("The arrays above should be [[], [1], [1, 2], [1, 2, 2], [2]], [2, 2]], \
+    [[], [1], [1, 2], [1, 2, 4], [1, 2, 4, 4], [1, 4], [1, 4, 4], [2], [2, 4], \
+        [2, 4, 4], [4], [4, 4]], and [[]].")
