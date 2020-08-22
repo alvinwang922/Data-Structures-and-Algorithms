471b3a3792f7ad3d469053188fd1ffeca90d3032
Alvin Wang <alvinwang922@gmail.com>
2020-08-22 16:28:06 -0700

    Determine if two strings are equal after backspace operations are done on both strings.

--- Strings/Backspace-String-Compare.py
+++ Strings/Backspace-String-Compare.py
@@ -1,6 +1,6 @@
 """
 Given two strings S and T, return if they are equal when both are typed 
-into empty text editors. # means a backspace character.
+into empty text editors. "#" means a backspace character.
 Note that after backspacing an empty text, the text will continue empty.
 """
 
