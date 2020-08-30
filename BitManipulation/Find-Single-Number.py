0cb2557e562cc4452098162b22e02a0c541dee61
Alvin Wang <alvinwang922@gmail.com>
2020-08-30 12:08:38 -0700

    Use bit manipulation to find the number in an array that only appears once.

--- BitManipulation/Find-Single-Number.py
+++ BitManipulation/Find-Single-Number.py
@@ -1,6 +1,8 @@
-# Given a non-empty array of integers, every
-# element appears twice except for one.
-# Find that single one.
+"""
+Given a non-empty array of integers, every
+element appears twice except for one.
+Find that single one.
+"""
 
 
 class Solution(object):
