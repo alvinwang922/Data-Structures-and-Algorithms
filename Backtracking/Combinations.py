2c4d849ff0abdb40aa2493844c4017d948e9d02c
Alvin Wang <alvinwang922@gmail.com>
2020-10-25 16:58:33 -0700

    Return all combinations of k numbers out of 1 to n.

---
+++ Backtracking/Combinations.py
@@ -0,0 +1,24 @@
+"""
+Given two integers n and k, return all possible combinations of 
+k numbers out of 1 ... n. You may return the answer in any order.
+"""
+
+
+class Solution:
+    def combine(self, n: int, k: int):
+        ans = []
+        self.backtrack(n, k, [], 1, ans)
+        return ans
+
+    def backtrack(self, n, k, curr, ind, ans):
+        if len(curr) == k:
+            ans.append(curr)
+        else:
+            for i in range(ind, n + 1):
+                self.backtrack(n, k, curr + [i], i + 1, ans)
+
+
+print(combine(4, 2))
+print(combine(1, 1))
+print("The arrays above should be [[1, 2], [1, 3], [1, 4], \
+    [2, 3], [2, 4], [3, 4]] and [[1]].")
