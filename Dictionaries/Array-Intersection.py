5c3db74839ae715f0b595013c49e7588e8f051de
Alvin Wang <alvinwang922@gmail.com>
2020-11-02 14:50:42 -0800

    Return the intersection of two arrays as an array.

--- Dictionaries/Array-Intersection.py
+++ Dictionaries/Array-Intersection.py
@@ -10,34 +10,18 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        count = defaultdict(int)
         ans = []
-        freq = defaultdict(int)
-        for i in range(len(nums1)):
-            freq[nums1[i]] += 1
-        for i in range(len(nums2)):
-            if freq[nums2[i]] > 0 and nums2[i] not in ans:
-                ans.append(nums2[i])
+        for i in nums1:
+            count[i] += 1
+        for j in nums2:
+            if j in count and count[j] > 0:
+                ans.append(j)
+                count[j] -= 1
         return ans
 
-    def main():
-        print(intersection([1, 2, 2, 1], [2, 2]))
-        print(intersection([4, 9, 5], [9, 4, 9, 8, 4]))
-        print(intersection([1, 3, 1], [2, 2]))
-        print("The arrays above should be [2], [9, 4], and [].")
 
-
-# A shorter solution
-class Solution(object):
-    def intersection(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        return list(set(nums1) & set(nums2))
-
-    def main():
-        print(intersection([1, 2, 2, 1], [2, 2]))
-        print(intersection([4, 9, 5], [9, 4, 9, 8, 4]))
-        print(intersection([1, 3, 1], [2, 2]))
-        print("The arrays above should be [2], [9, 4], and [].")
+print(intersection([1, 2, 2, 1], [2, 2]))
+print(intersection([4, 9, 5], [9, 4, 9, 8, 4]))
+print(intersection([1, 3, 1], [2, 2]))
+print("The arrays above should be [2], [9, 4], and [].")
