e1ca7c0cb9581cc992d1351d5369e8cdc2811b6f
Alvin Wang <alvinwang922@gmail.com>
2020-12-01 11:59:29 -0800

    Flatten a binary tree into a linked list in-place.

---
+++ Trees/Flatten-Binary-Tree.py
@@ -0,0 +1,51 @@
+"""
+Given a binary tree, flatten it to a linked list in-place.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    nxt = None
+
+    def flatten(self, root: TreeNode):
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if root is None:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.right = self.nxt
+        root.left = None
+        self.nxt = root
+
+
+"""
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+"""
