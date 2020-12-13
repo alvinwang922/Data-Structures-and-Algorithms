3aae0bd1d97bab6ae86b58771b0c1af2a58e0600
Alvin Wang <alvinwang922@gmail.com>
2020-12-13 15:34:39 -0800

    Return the array such that duplicates can only appear at most twice.

---
+++ Arrays/Remove-Duplicates-Two.py
@@ -0,0 +1,24 @@
+"""
+Given a sorted array nums, remove the duplicates 
+in-place such that duplicates appeared at most 
+twice and return the new length. Do not allocate 
+extra space for another array; you must do this 
+by modifying the input array in-place with O(1) 
+extra memory.
+"""
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]):
+        ans = 0
+        for num in nums:
+            if ans < 2 or num != nums[ans - 2]:
+                nums[ans] = num
+                ans += 1
+        return nums[:ans]
+
+    print(removeDuplicates([0, 1]))
+    print(removeDuplicates([1, 1, 1, 2, 2, 3]))
+    print(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+    print("The arrays above should be [0, 1], \
+        [1, 1, 2, 2, 3], and [0, 0, 1, 1, 2, 3, 3].")
