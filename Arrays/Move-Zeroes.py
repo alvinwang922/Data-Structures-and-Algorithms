2c40b8bd9238e628e748f3a5a83896c2a0f95ba7
Alvin Wang <alvinwang922@gmail.com>
2021-02-18 16:06:44 -0800

    Given an array, move all the zeroes to the end.

--- Arrays/Move-Zeroes.py
+++ Arrays/Move-Zeroes.py
@@ -1,8 +1,10 @@
-# Given an array nums, write a function to move all 0's
-# to the end of it while maintaining the relative order
-# of the non-zero elements.
-# You must do this in-place without making a copy of the array.
-# Minimize the total number of operations.
+"""
+Given an array nums, write a function to move all 0's
+to the end of it while maintaining the relative order
+of the non-zero elements.
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
 
 
 class Solution(object):
@@ -20,18 +22,17 @@ class Solution(object):
             nums[len(nums) - counter] = 0
             counter -= 1
 
+    # Another solution
 
-# Another solution
-class Solution(object):
-    def moveZeroes(self, nums):
+    def moveZeroes2(self, nums):
         pointer = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[i], nums[pointer] = nums[pointer], nums[i]
                 pointer += 1
 
-
-print(moveZeroes([0, 1, 0, 3, 12]))
-print(moveZeroes([0, 1, 0]))
-print(moveZeroes([0, 0, 1, 1]))
-print("The values above should be [1,3,12,0,0], [1,0,0], and [1,1,0,0].")
+    print(moveZeroes([0, 1, 0, 3, 12]))
+    print(moveZeroes([0, 1, 0]))
+    print(moveZeroes([0, 0, 1, 1]))
+    print("The values above should be [1, 3, 12, 0, 0], [1, 0, 0], \
+        and [1, 1, 0, 0].")
