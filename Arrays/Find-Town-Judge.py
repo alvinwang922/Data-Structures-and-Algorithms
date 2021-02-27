bbf34b8cec8d8d2f550909d42e9a2c9cd7e8f1c8
Alvin Wang <alvinwang922@gmail.com>
2021-02-26 16:51:02 -0800

    Identify the town judge given who each person trusts.

--- Arrays/Find-Town-Judge.py
+++ Arrays/Find-Town-Judge.py
@@ -1,13 +1,15 @@
-# In a town, there are N people labelled from 1 to N. There
-# is a rumor that one of these people is secretly the town judge.
-# If the town judge exists, then:
-# The town judge trusts nobody.
-# Everybody (except for the town judge) trusts the town judge.
-# There is exactly one person that satisfies properties 1 and 2.
-# You are given trust, an array of pairs trust[i] = [a, b]
-# representing that the person labelled a trusts the person labelled b.
-# If the town judge exists and can be identified, return the label of
-# the town judge.  Otherwise, return -1.
+"""
+In a town, there are N people labelled from 1 to N. There
+is a rumor that one of these people is secretly the town judge.
+If the town judge exists, then:
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given trust, an array of pairs trust[i] = [a, b]
+representing that the person labelled a trusts the person labelled b.
+If the town judge exists and can be identified, return the label of
+the town judge.  Otherwise, return -1.
+"""
 
 
 class Solution(object):
@@ -27,10 +29,9 @@ class Solution(object):
                     return j + 1
         return -1
 
-
-print(findJudge(2, [[1, 2]])
-print(findJudge(3, [[1, 3], [2, 3]])
-print(findJudge(3, [[[1, 3], [2, 3], [3, 1]])
-print(findJudge(3, [[1, 2], [2, 3]])
-print(findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]])
-print("The values above should be 2, 3, -1, -1, and 3")
+    print(findJudge(2, [[1, 2]]))
+    print(findJudge(3, [[1, 3], [2, 3]]))
+    print(findJudge(3, [[[1, 3], [2, 3], [3, 1]]]))
+    print(findJudge(3, [[1, 2], [2, 3]]))
+    print(findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]]))
+    print("The values above should be 2, 3, -1, -1, and 3")
