c7e557bed2295bb8dd7e4968207f0f3761d2aab8
Alvin Wang <alvinwang922@gmail.com>
2021-03-03 15:13:42 -0800

    Determine the majority element in a given array.

--- Arrays/Majority-Element.py
+++ Arrays/Majority-Element.py
@@ -1,8 +1,10 @@
-# Given an array of size n, find the majority element.
-# The majority element is the element that appears more
-# than ⌊ n/2 ⌋ times.
-# You may assume that the array is non-empty and the
-# majority element always exist in the array.
+"""
+Given an array of size n, find the majority element.
+The majority element is the element that appears more
+than ⌊ n/2 ⌋ times.
+You may assume that the array is non-empty and the
+majority element always exist in the array.
+"""
 
 
 class Solution(object):
@@ -19,11 +21,9 @@ class Solution(object):
             else:
                 counter = 0
 
+    # Better solution in O(n) instead of O(nlogn)
 
-# Better solution in O(n) instead of O(nlogn)
-
-class Solution(object):
-    def majorityElement(self, nums):
+    def majorityElement2(self, nums):
         elements = dict()
         for i in range(len(nums)):
             elements[nums[i]] = elements.get(nums[i], 0) + 1
@@ -31,8 +31,7 @@ class Solution(object):
             if elements[key] > len(nums) // 2:
                 return key
 
-
-print(majorityElement([3, 2, 3]))
-print(majorityElement([2, 2, 1, 1, 1, 2, 2]))
-print(majorityElement([1]))
-print("The values above should be 3, 2, and 1.")
+    print(majorityElement([3, 2, 3]))
+    print(majorityElement([2, 2, 1, 1, 1, 2, 2]))
+    print(majorityElement([1]))
+    print("The values above should be 3, 2, and 1.")
