7bb355928305e7a184370f56e3f54e57051a6514
Alvin Wang <alvinwang922@gmail.com>
2021-03-10 14:58:11 -0800

    Return the minimum number of interval removals required to get rid of all overlaps.

--- Arrays/Non-Overlapping-Intervals.py
+++ Arrays/Non-Overlapping-Intervals.py
@@ -1,6 +1,6 @@
 """
-Given a collection of intervals, find the minimum number of intervals you need 
-to remove to make the rest of the intervals non-overlapping.
+Given a collection of intervals, find the minimum number of intervals 
+you need to remove to make the rest of the intervals non-overlapping.
 """
 
 
@@ -15,8 +15,7 @@ class Solution:
                 removed += 1
         return removed
 
-
-print(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
-print(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]))
-print(eraseOverlapIntervals([[1, 2], [2, 3]]))
-print("The integers above should be 1, 2, and 0.")
+    print(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
+    print(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]))
+    print(eraseOverlapIntervals([[1, 2], [2, 3]]))
+    print("The integers above should be 1, 2, and 0.")
