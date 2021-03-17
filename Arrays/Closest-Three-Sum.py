f9664e60bb23a33f67dbc6784a26f5dedaa51eb4
Alvin Wang <alvinwang922@gmail.com>
2021-03-17 14:38:36 -0700

    Return the sum of three integers in an array closest to a target value.

--- Arrays/Closest-Three-Sum.py
+++ Arrays/Closest-Three-Sum.py
@@ -6,26 +6,26 @@ input would have exactly one solution.
 """
 
 
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int):
-        nums.sort()
-        ans = nums[0] + nums[1] + nums[2]
-        l = len(nums)
-        for i in range(l - 2):
-            left, right = i + 1, l - 1
-            while left < right:
-                currSum = nums[i] + nums[left] + nums[right]
-                if currSum == target:
-                    return currSum
-                if abs(currSum - target) < abs(ans - target):
-                    ans = currSum
-                if currSum < target:
-                    left += 1
-                elif currSum > target:
-                    right -= 1
-        return ans
+def threeSumClosest(nums: List[int], target: int):
+    nums.sort()
+    ans = nums[0] + nums[1] + nums[2]
+    l = len(nums)
+    for i in range(l - 2):
+        left, right = i + 1, l - 1
+        while left < right:
+            currSum = nums[i] + nums[left] + nums[right]
+            if currSum == target:
+                return currSum
+            if abs(currSum - target) < abs(ans - target):
+                ans = currSum
+            if currSum < target:
+                left += 1
+            elif currSum > target:
+                right -= 1
+    return ans
 
-    print(threeSumClosest([-1, 2, 1, -4], 1))
-    print(threeSumClosest([4, 2, 3, 2], 8))
-    print(threeSumClosest([5, 6, 7], 2))
-    print("The values above should be 2, 8, and 18.")
+
+print(threeSumClosest([-1, 2, 1, -4], 1))
+print(threeSumClosest([4, 2, 3, 2], 8))
+print(threeSumClosest([5, 6, 7], 2))
+print("The values above should be 2, 8, and 18.")
