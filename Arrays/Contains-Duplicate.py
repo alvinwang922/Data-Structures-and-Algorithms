e80fdf5c34c65b29f0909da05f20140e936b8ff9
Alvin Wang <alvinwang922@gmail.com>
2021-03-19 18:05:42 -0700

    Determine if an array contains any duplicates.

--- Arrays/Contains-Duplicate.py
+++ Arrays/Contains-Duplicate.py
@@ -6,11 +6,11 @@ false if every element is distinct.
 """
 
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]):
-        return len(nums) != len(set(nums))
+def containsDuplicate(self, nums: List[int]):
+    return len(nums) != len(set(nums))
 
-    print(containsDuplicate([1, 2, 3, 1]))
-    print(containsDuplicate([1, 2, 3, 4]))
-    print(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
-    print("The booleans above should be True, False, and True.")
+
+print(containsDuplicate([1, 2, 3, 1]))
+print(containsDuplicate([1, 2, 3, 4]))
+print(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+print("The booleans above should be True, False, and True.")
