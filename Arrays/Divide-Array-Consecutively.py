2d6ffb96fc77e25c2f4d592b9f5a3a75ecc6c896
Alvin Wang <alvinwang922@gmail.com>
2021-03-21 14:07:46 -0700

    Determine whether an array can be divided into consecutive sets of length k.

--- Arrays/Divide-Array-Consecutively.py
+++ Arrays/Divide-Array-Consecutively.py
@@ -7,22 +7,22 @@ Return True if its possible otherwise return False.
 import collections
 
 
-class Solution:
-    def isPossibleDivide(self, nums: List[int], k: int):
-        if len(nums) % k != 0:
-            return False
-        count = collections.Counter(nums)
-        keys = sorted(count.keys())
-        for n in keys:
-            if count[n] > 0:
-                start = count[n]
-                for i in range(n, n+k):
-                    if count[i] < start:
-                        return False
-                    count[i] -= start
-        return True
+def isPossibleDivide(self, nums: List[int], k: int):
+    if len(nums) % k != 0:
+        return False
+    count = collections.Counter(nums)
+    keys = sorted(count.keys())
+    for n in keys:
+        if count[n] > 0:
+            start = count[n]
+            for i in range(n, n+k):
+                if count[i] < start:
+                    return False
+                count[i] -= start
+    return True
 
-    print(isPossibleDivide([3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3))
-    print(isPossibleDivide([3, 3, 2, 2, 1, 1], 3))
-    print(isPossibleDivide([1, 2, 3, 4], 3))
-    print("The booleans above should be True, True, and False.")
+
+print(isPossibleDivide([3, 2, 1, 2, 3, 4, 3, 4, 5, 9, 10, 11], 3))
+print(isPossibleDivide([3, 3, 2, 2, 1, 1], 3))
+print(isPossibleDivide([1, 2, 3, 4], 3))
+print("The booleans above should be True, True, and False.")
