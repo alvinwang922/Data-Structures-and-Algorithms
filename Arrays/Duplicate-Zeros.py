02421a58926f89a77cdd11cf80d4bafb0ffd3f52
Alvin Wang <alvinwang922@gmail.com>
2021-03-23 14:57:30 -0700

    Duplicate each occurrence of zero in an array of fixed length and shift the remaining elements to the right.

--- Arrays/Duplicate-Zeros.py
+++ Arrays/Duplicate-Zeros.py
@@ -7,24 +7,24 @@ original array are not written.
 """
 
 
-class Solution:
-    def duplicateZeros(self, arr: List[int]):
-        zeros = 0
-        for num in arr:
-            if num == 0:
-                zeros += 1
-        length = len(arr)
-        for i in range(length - 1, -1, -1):
+def duplicateZeros(arr: List[int]):
+    zeros = 0
+    for num in arr:
+        if num == 0:
+            zeros += 1
+    length = len(arr)
+    for i in range(length - 1, -1, -1):
+        if i + zeros < length:
+            arr[i + zeros] = arr[i]
+        if arr[i] == 0:
+            zeros -= 1
             if i + zeros < length:
-                arr[i + zeros] = arr[i]
-            if arr[i] == 0:
-                zeros -= 1
-                if i + zeros < length:
-                    arr[i + zeros] = 0
-        return arr
+                arr[i + zeros] = 0
+    return arr
 
-    print(duplicateZeros([0]))
-    print(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]))
-    print(duplicateZeros([1, 2, 3]))
-    print("The lists above should be [0], \
-        [1, 0, 0, 2, 3, 0, 0, 4], and [1, 2, 3].")
+
+print(duplicateZeros([0]))
+print(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]))
+print(duplicateZeros([1, 2, 3]))
+print("The lists above should be [0], \
+    [1, 0, 0, 2, 3, 0, 0, 4], and [1, 2, 3].")
