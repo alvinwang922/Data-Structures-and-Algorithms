58819c15983a0e7ac1da0f9a16463dcfb5a1e611
Alvin Wang <alvinwang922@gmail.com>
2021-03-29 12:27:29 -0700

    Return whether or not a trip can be made given gas station costs.

--- Arrays/Gas-Station.py
+++ Arrays/Gas-Station.py
@@ -10,26 +10,26 @@ once in the clockwise direction, otherwise return -1.
 """
 
 
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
-        if len(gas) == 0 or len(cost) == 0 or sum(gas) < sum(cost):
-            return -1
-        else:
-            position = 0
-            balance = 0
-            for i in range(len(gas)):
-                balance += gas[i] - cost[i]
-                if balance < 0:
-                    balance = 0
-                    position = i + 1
-            return position
+def canCompleteCircuit(gas, cost):
+    """
+    :type gas: List[int]
+    :type cost: List[int]
+    :rtype: int
+    """
+    if len(gas) == 0 or len(cost) == 0 or sum(gas) < sum(cost):
+        return -1
+    else:
+        position = 0
+        balance = 0
+        for i in range(len(gas)):
+            balance += gas[i] - cost[i]
+            if balance < 0:
+                balance = 0
+                position = i + 1
+        return position
 
-    print(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
-    print(canCompleteCircuit([2, 3, 4], [3, 4, 3]))
-    print(canCompleteCircuit([], [1]))
-    print("The values above should be 3, -1, and -1.")
+
+print(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
+print(canCompleteCircuit([2, 3, 4], [3, 4, 3]))
+print(canCompleteCircuit([], [1]))
+print("The values above should be 3, -1, and -1.")
