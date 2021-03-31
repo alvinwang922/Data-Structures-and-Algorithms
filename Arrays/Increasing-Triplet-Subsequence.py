692cdb838c2cd80b735bab7767bd4c16a7b1ebb7
Alvin Wang <alvinwang922@gmail.com>
2021-03-31 12:29:56 -0700

    Return whether or not there is an increasing subsequence of length three in an array.

--- Arrays/Increasing-Triplet-Subsequence.py
+++ Arrays/Increasing-Triplet-Subsequence.py
@@ -4,33 +4,34 @@ subsequence of length 3 exists or not in the array.
 """
 
 
-class Solution:
-    def increasingTriplet(self, nums: List[int]):
-        if len(nums) < 3:
-            return False
-        start, mid = nums[0], float("inf")
-        for i in range(1, len(nums)):
-            if nums[i] < start:
-                start = nums[i]
-            elif start < nums[i] < mid:
-                mid = nums[i]
-            if nums[i] > mid:
-                return True
+def increasingTriplet(self, nums: List[int]):
+    if len(nums) < 3:
         return False
+    start, mid = nums[0], float("inf")
+    for i in range(1, len(nums)):
+        if nums[i] < start:
+            start = nums[i]
+        elif start < nums[i] < mid:
+            mid = nums[i]
+        if nums[i] > mid:
+            return True
+    return False
 
-    # A cleaner solution
-    def increasingTriplet2(self, nums: List[int]):
-        start = mid = float("inf")
-        for n in nums:
-            if n <= start:
-                start = n
-            elif n <= mid:
-                mid = n
-            else:
-                return True
-        return False
 
-    print(increasingTriplet([1, 2]))
-    print(increasingTriplet([1, 2, 3, 4, 5]))
-    print(increasingTriplet([5, 4, 3, 2, 1]))
-    print("The booleans above should be False, True, and False.")
+# A cleaner solution
+def increasingTriplet2(self, nums: List[int]):
+    start = mid = float("inf")
+    for n in nums:
+        if n <= start:
+            start = n
+        elif n <= mid:
+            mid = n
+        else:
+            return True
+    return False
+
+
+print(increasingTriplet([1, 2]))
+print(increasingTriplet([1, 2, 3, 4, 5]))
+print(increasingTriplet([5, 4, 3, 2, 1]))
+print("The booleans above should be False, True, and False.")
