c817efd8fd46c9276ddcfc3b99e8c22f37d17da3
Alvin Wang <alvinwang922@gmail.com>
2021-04-02 16:22:46 -0700

    Determine the new array of intervals after inserting and merging a new interval.

--- Arrays/Insert-Interval.py
+++ Arrays/Insert-Interval.py
@@ -5,24 +5,24 @@ initially sorted according to their start times.
 """
 
 
-class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]):
-        front = []
-        back = []
-        intervalStart = newInterval[0]
-        intervalEnd = newInterval[1]
-        for i in intervals:
-            if i[1] < newInterval[0]:
-                front.append(i)
-            elif i[0] > newInterval[1]:
-                back.append(i)
-            else:
-                intervalStart = min(intervalStart, i[0])
-                intervalEnd = max(intervalEnd, i[1])
-        return front + [[intervalStart, intervalEnd]] + back
+def insert(self, intervals: List[List[int]], newInterval: List[int]):
+    front = []
+    back = []
+    intervalStart = newInterval[0]
+    intervalEnd = newInterval[1]
+    for i in intervals:
+        if i[1] < newInterval[0]:
+            front.append(i)
+        elif i[0] > newInterval[1]:
+            back.append(i)
+        else:
+            intervalStart = min(intervalStart, i[0])
+            intervalEnd = max(intervalEnd, i[1])
+    return front + [[intervalStart, intervalEnd]] + back
 
-    print(insert([[1, 3], [6, 9]], [2, 5]))
-    print(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
-    print(insert([[1, 5]], [1, 3]))
-    print("The arrays above should be [[1, 5], [6, 9]], \
-        [[1, 2], [3, 10], [12, 16]], and [[1, 5]]")
+
+print(insert([[1, 3], [6, 9]], [2, 5]))
+print(insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
+print(insert([[1, 5]], [1, 3]))
+print("The arrays above should be [[1, 5], [6, 9]], \
+    [[1, 2], [3, 10], [12, 16]], and [[1, 5]]")
