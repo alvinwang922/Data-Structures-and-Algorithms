30093c716e45b9748927987201fe1f861c902e78
Alvin Wang <alvinwang922@gmail.com>
2021-04-03 16:23:23 -0700

    Determine the interval intersections of two lists of intervals.

--- Arrays/Interval-Intersections.py
+++ Arrays/Interval-Intersections.py
@@ -9,23 +9,23 @@ closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
 """
 
 
-class Solution:
-    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]):
-        a, b, intervals = 0, 0, []
-        while a < len(A) and b < len(B):
-            aInterval = A[a]
-            bInterval = B[b]
-            start = max(aInterval[0], bInterval[0])
-            end = min(aInterval[1], bInterval[1])
-            if end >= start:
-                intervals.append([start, end])
-            if aInterval[1] >= bInterval[1]:
-                b += 1
-            else:
-                a += 1
-        return intervals
+def intervalIntersection(self, A: List[List[int]], B: List[List[int]]):
+    a, b, intervals = 0, 0, []
+    while a < len(A) and b < len(B):
+        aInterval = A[a]
+        bInterval = B[b]
+        start = max(aInterval[0], bInterval[0])
+        end = min(aInterval[1], bInterval[1])
+        if end >= start:
+            intervals.append([start, end])
+        if aInterval[1] >= bInterval[1]:
+            b += 1
+        else:
+            a += 1
+    return intervals
 
-    print(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]],
-                               [[1, 5], [8, 12], [15, 24], [25, 26]]))
-    print("The intervals above should be [[1, 2], [5, 5], [8, 10], \
-        [15, 23], [24, 24], [25, 25]].")
+
+print(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]],
+                           [[1, 5], [8, 12], [15, 24], [25, 26]]))
+print("The intervals above should be [[1, 2], [5, 5], [8, 10], \
+    [15, 23], [24, 24], [25, 25]].")
