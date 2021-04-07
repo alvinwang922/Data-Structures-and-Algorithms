c956df0ec4bfbeaa035aa13625a31f3dbb782c8c
Alvin Wang <alvinwang922@gmail.com>
2021-04-06 22:43:08 -0700

    Determine the majority element in a given array.

--- Arrays/Majority-Element.py
+++ Arrays/Majority-Element.py
@@ -7,31 +7,32 @@ majority element always exist in the array.
 """
 
 
-class Solution(object):
-    def majorityElement(self, nums):
-        if len(nums) < 3:
-            return nums[0]
-        counter = 0
-        nums = sorted(nums)
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i+1]:
-                counter += 1
-                if counter >= len(nums)/2:
-                    return nums[i]
-            else:
-                counter = 0
+def majorityElement(self, nums):
+    if len(nums) < 3:
+        return nums[0]
+    counter = 0
+    nums = sorted(nums)
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i+1]:
+            counter += 1
+            if counter >= len(nums)/2:
+                return nums[i]
+        else:
+            counter = 0
 
-    # Better solution in O(n) instead of O(nlogn)
+# Better solution in O(n) instead of O(nlogn)
 
-    def majorityElement2(self, nums):
-        elements = dict()
-        for i in range(len(nums)):
-            elements[nums[i]] = elements.get(nums[i], 0) + 1
-        for key in elements:
-            if elements[key] > len(nums) // 2:
-                return key
 
-    print(majorityElement([3, 2, 3]))
-    print(majorityElement([2, 2, 1, 1, 1, 2, 2]))
-    print(majorityElement([1]))
-    print("The values above should be 3, 2, and 1.")
+def majorityElement2(self, nums):
+    elements = dict()
+    for i in range(len(nums)):
+        elements[nums[i]] = elements.get(nums[i], 0) + 1
+    for key in elements:
+        if elements[key] > len(nums) // 2:
+            return key
+
+
+print(majorityElement([3, 2, 3]))
+print(majorityElement([2, 2, 1, 1, 1, 2, 2]))
+print(majorityElement([1]))
+print("The values above should be 3, 2, and 1.")
