795b4ada5485b85b68d604ca85462c5738b74686
Alvin Wang <alvinwang922@gmail.com>
2021-04-08 03:02:19 -0700

    Return the kth largest element in an unsorted array.

--- Arrays/Kth-Largest-Element.py
+++ Arrays/Kth-Largest-Element.py
@@ -5,12 +5,12 @@ sorted order, not the kth distinct element.
 """
 
 
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int):
-        nums.sort()
-        return nums[-k]
+def findKthLargest(nums: List[int], k: int):
+    nums.sort()
+    return nums[-k]
 
-    print(findKthLargest([1, 2, 3], 3))
-    print(findKthLargest([3, 2, 1, 5, 6, 4], 2))
-    print(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
-    print("The values above should be 1, 4, and 5.")
+
+print(findKthLargest([1, 2, 3], 3))
+print(findKthLargest([3, 2, 1, 5, 6, 4], 2))
+print(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+print("The values above should be 1, 4, and 5.")
