fe8c4dbda33a36644ac05c9faab567343ee7df00
Alvin Wang <alvinwang922@gmail.com>
2021-04-08 02:59:43 -0700

    Return the max number of consecutive ones in a binary array.

--- Arrays/Max-Consecutive-Ones.py
+++ Arrays/Max-Consecutive-Ones.py
@@ -4,18 +4,18 @@ consecutive 1s in this array.
 """
 
 
-class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]):
-        curr, ans = 0, 0
-        for num in nums:
-            if num == 1:
-                curr += 1
-                ans = max(ans, curr)
-            else:
-                curr = 0
-        return ans
+def findMaxConsecutiveOnes(nums: List[int]):
+    curr, ans = 0, 0
+    for num in nums:
+        if num == 1:
+            curr += 1
+            ans = max(ans, curr)
+        else:
+            curr = 0
+    return ans
 
-    print(findMaxConsecutiveOnes([0, 0]))
-    print(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
-    print(findMaxConsecutiveOnes([1, 0, 1]))
-    print("The values above should be 0, 3, and 1.")
+
+print(findMaxConsecutiveOnes([0, 0]))
+print(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
+print(findMaxConsecutiveOnes([1, 0, 1]))
+print("The values above should be 0, 3, and 1.")
