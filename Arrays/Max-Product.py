cf14e982d733d0000718498d56420397f1de5ece
Alvin Wang <alvinwang922@gmail.com>
2021-04-09 15:15:21 -0700

    Return the maximum product of a subarray given an array.

--- Arrays/Max-Product.py
+++ Arrays/Max-Product.py
@@ -5,23 +5,23 @@ has the largest product.
 """
 
 
-class Solution(object):
-    def maxProduct(self, nums):
-        currentMax = nums[0]
-        currentMin = nums[0]
-        maxValue = currentMax
-        for i in range(1, len(nums)):
-            if nums[i] >= 0:
-                currentMax = max(nums[i], nums[i]*currentMax)
-                currentMin = min(nums[i], nums[i]*currentMin)
-            if nums[i] < 0:
-                temp = max(nums[i], nums[i]*currentMin)
-                currentMin = min(nums[i], nums[i]*currentMax)
-                currentMax = temp
-            maxValue = max(currentMax, maxValue)
-        return maxValue
+def maxProduct(nums):
+    currentMax = nums[0]
+    currentMin = nums[0]
+    maxValue = currentMax
+    for i in range(1, len(nums)):
+        if nums[i] >= 0:
+            currentMax = max(nums[i], nums[i] * currentMax)
+            currentMin = min(nums[i], nums[i] * currentMin)
+        if nums[i] < 0:
+            temp = max(nums[i], nums[i] * currentMin)
+            currentMin = min(nums[i], nums[i] * currentMax)
+            currentMax = temp
+        maxValue = max(currentMax, maxValue)
+    return maxValue
 
-    print(maxProduct([-2, 3, -4, 5, -6]))
-    print(maxProduct([2, 3, -2, 4]))
-    print(maxProduct([-2, 0, -1]))
-    print("The values above should be 360, 6, and 0.")
+
+print(maxProduct([-2, 3, -4, 5, -6]))
+print(maxProduct([2, 3, -2, 4]))
+print(maxProduct([-2, 0, -1]))
+print("The values above should be 360, 6, and 0.")
