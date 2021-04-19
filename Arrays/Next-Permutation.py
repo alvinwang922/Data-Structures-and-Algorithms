1d714e0f4d2348f6bac51f2033c14deef3d433c6
Alvin Wang <alvinwang922@gmail.com>
2021-04-18 21:46:52 -0700

    Determine the next largest permutation of a list of integers.

--- Arrays/Next-Permutation.py
+++ Arrays/Next-Permutation.py
@@ -7,26 +7,26 @@ must be in-place and use only constant extra memory.
 """
 
 
-class Solution:
-    def nextPermutation(self, nums: List[int]):
-        # Do not return anything, modify nums in-place instead.
-        i = j = len(nums) - 1
-        while i > 0 and nums[i] <= nums[i - 1]:
-            i -= 1
-        if i == 0:
-            nums.reverse()
-        else:
-            k = i - 1
-            while nums[k] >= nums[j]:
-                j -= 1
-            nums[k], nums[j] = nums[j], nums[k]
-            front, end = k + 1, len(nums) - 1
-            while front < end:
-                nums[front], nums[end] = nums[end], nums[front]
-                front += 1
-                end -= 1
+def nextPermutation(nums: List[int]):
+    # Do not return anything, modify nums in-place instead.
+    i = j = len(nums) - 1
+    while i > 0 and nums[i] <= nums[i - 1]:
+        i -= 1
+    if i == 0:
+        nums.reverse()
+    else:
+        k = i - 1
+        while nums[k] >= nums[j]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]
+        front, end = k + 1, len(nums) - 1
+        while front < end:
+            nums[front], nums[end] = nums[end], nums[front]
+            front += 1
+            end -= 1
 
-    print(nextPermutation([1, 2, 3]))
-    print(nextPermutation([3, 2, 1]))
-    print(nextPermutation([1, 1, 5]))
-    print("The arrays above should be [1, 3, 2], [1, 2, 3], and [1, 5, 1].")
+
+print(nextPermutation([1, 2, 3]))
+print(nextPermutation([3, 2, 1]))
+print(nextPermutation([1, 1, 5]))
+print("The arrays above should be [1, 3, 2], [1, 2, 3], and [1, 5, 1].")
