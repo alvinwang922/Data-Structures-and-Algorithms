f71ad82fdbfbb880c2899bfbac00c335866e8cb5
Alvin Wang <alvinwang922@gmail.com>
2021-04-19 15:02:03 -0700

    Return the minimum number of interval removals required to get rid of all overlaps.

--- Arrays/Non-Overlapping-Intervals.py
+++ Arrays/Non-Overlapping-Intervals.py
@@ -4,18 +4,18 @@ you need to remove to make the rest of the intervals non-overlapping.
 """
 
 
-class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        end = float('-inf')
-        removed = 0
-        for i in sorted(intervals, key=lambda i: i[1]):
-            if i[0] >= end:
-                end = i[1]
-            else:
-                removed += 1
-        return removed
+def eraseOverlapIntervals(intervals: List[List[int]]) -> int:
+    end = float('-inf')
+    removed = 0
+    for i in sorted(intervals, key=lambda i: i[1]):
+        if i[0] >= end:
+            end = i[1]
+        else:
+            removed += 1
+    return removed
 
-    print(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
-    print(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]))
-    print(eraseOverlapIntervals([[1, 2], [2, 3]]))
-    print("The integers above should be 1, 2, and 0.")
+
+print(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]]))
+print(eraseOverlapIntervals([[1, 2], [1, 2], [1, 2]]))
+print(eraseOverlapIntervals([[1, 2], [2, 3]]))
+print("The integers above should be 1, 2, and 0.")
