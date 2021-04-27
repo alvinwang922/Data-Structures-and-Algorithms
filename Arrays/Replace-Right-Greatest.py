76ca63769d44987226364a32026fee29d9f56eec
Alvin Wang <alvinwang922@gmail.com>
2021-04-27 15:23:45 -0700

    Replace elements in an array with the greatest element to its right.

--- Arrays/Replace-Right-Greatest.py
+++ Arrays/Replace-Right-Greatest.py
@@ -6,16 +6,16 @@ After doing so, return the array.
 """
 
 
-class Solution:
-    def replaceElements(self, arr: List[int]):
-        currMax = -1
-        for i in range(len(arr) - 1, -1, -1):
-            arr[i], currMax = currMax, \
-                max(currMax, arr[i])
-        return arr
+def replaceElements(arr: List[int]):
+    currMax = -1
+    for i in range(len(arr) - 1, -1, -1):
+        arr[i], currMax = currMax, \
+            max(currMax, arr[i])
+    return arr
 
-    print(replaceElements([1]))
-    print(replaceElements([17, 18, 5, 4, 6, 1]))
-    print(replaceElements([5, 4, 3, 2, 1]))
-    print("The arrays above should be [-1], \
-        [18, 6, 6, 6, 1, -1], and [4, 3, 2, 1, -1].")
+
+print(replaceElements([1]))
+print(replaceElements([17, 18, 5, 4, 6, 1]))
+print(replaceElements([5, 4, 3, 2, 1]))
+print("The arrays above should be [-1], \
+    [18, 6, 6, 6, 1, -1], and [4, 3, 2, 1, -1].")
