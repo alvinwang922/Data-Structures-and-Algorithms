8e8a857a060c0ae8057800413cc518db58b1eb6a
Alvin Wang <alvinwang922@gmail.com>
2021-05-02 15:48:30 -0700

    Find the number with occurrence of one.

--- Arrays/Single-Number-Two.py
+++ Arrays/Single-Number-Two.py
@@ -5,18 +5,17 @@ which appears exactly once. Find that single one.
 """
 
 
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        occurrence = dict()
-        for i in range(len(nums)):
-            occurrence[nums[i]] = occurrence.get(nums[i], 0) + 1
-        for key in occurrence:
-            if occurrence[key] == 1:
-                return key
+def singleNumber(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    occurrence = dict()
+    for i in range(len(nums)):
+        occurrence[nums[i]] = occurrence.get(nums[i], 0) + 1
+    for key in occurrence:
+        if occurrence[key] == 1:
+            return key
 
 
 print(singleNumber([2, 2, 3, 2]))
