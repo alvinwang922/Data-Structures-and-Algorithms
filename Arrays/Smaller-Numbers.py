b504bee402b18cea3cb80a81f7ee2bfcec4eb11b
Alvin Wang <alvinwang922@gmail.com>
2021-05-06 16:17:33 -0700

    Given an array, return how many numbers are smaller than each number in it.

--- Arrays/Smaller-Numbers.py
+++ Arrays/Smaller-Numbers.py
@@ -6,25 +6,27 @@ Return the answer in an array.
 """
 
 
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]):
-        count = Counter(nums)
-        tracker = defaultdict(int)
-        sort = sorted(nums)
-        curr = 0
-        visited = set()
-        for num in sort:
-            if num not in visited:
-                tracker[num] = curr
-                curr += count[num]
-                visited.add(num)
-        for i in range(len(nums)):
-            nums[i] = tracker[nums[i]]
-        return nums
+from collections import Counter, defaultdict
+
+
+def smallerNumbersThanCurrent(nums: List[int]):
+    count = Counter(nums)
+    tracker = defaultdict(int)
+    sort = sorted(nums)
+    curr = 0
+    visited = set()
+    for num in sort:
+        if num not in visited:
+            tracker[num] = curr
+            curr += count[num]
+            visited.add(num)
+    for i in range(len(nums)):
+        nums[i] = tracker[nums[i]]
+    return nums
 
 
 print(smallerNumbersThanCurrent([8, 1, 2, 2, 3]))
 print(smallerNumbersThanCurrent([6, 5, 4, 8]))
 print(smallerNumbersThanCurrent([7, 7, 7, 7]))
 print("The arrays above should be [4, 0, 1, 1, 3], [2, 1, 0, 3], \
-    and [0, 0, 0, 0].")
+and [0, 0, 0, 0].")
