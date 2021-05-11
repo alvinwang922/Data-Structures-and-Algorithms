783f5ef006fb41f653da7194c8f1f865f87376b0
Alvin Wang <alvinwang922@gmail.com>
2021-05-10 22:33:17 -0700

    Determine the closest sum three numbers in an array are to a target value.

--- Arrays/Three-Sum-Closest.py
+++ Arrays/Three-Sum-Closest.py
@@ -5,32 +5,31 @@ integers. You may assume that each input would have exactly one solution.
 """
 
 
-class Solution:
-    def threeSumClosest(self, nums: List[int], target: int):
-        if len(nums) <= 3:
-            return sum(nums)
-        nums.sort()
-        tempAns = nums[0] + nums[1] + nums[2]
-        ans = tempAns
-        print(nums)
-        for i in range(len(nums) - 2):
-            if i == 0 or nums[i] != nums[i - 1]:
-                left, right = i + 1, len(nums) - 1
-                while left < right:
-                    tempAns = nums[i] + nums[left] + nums[right]
-                    if tempAns < target:
-                        while left < right and nums[left] == nums[left + 1]:
-                            left += 1
+def threeSumClosest(nums: List[int], target: int):
+    if len(nums) <= 3:
+        return sum(nums)
+    nums.sort()
+    tempAns = nums[0] + nums[1] + nums[2]
+    ans = tempAns
+    print(nums)
+    for i in range(len(nums) - 2):
+        if i == 0 or nums[i] != nums[i - 1]:
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                tempAns = nums[i] + nums[left] + nums[right]
+                if tempAns < target:
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    elif tempAns > target:
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
+                    left += 1
+                elif tempAns > target:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
-                    else:
-                        return tempAns
-                    if abs(target - tempAns) < abs(target - ans):
-                        ans = tempAns
-        return ans
+                    right -= 1
+                else:
+                    return tempAns
+                if abs(target - tempAns) < abs(target - ans):
+                    ans = tempAns
+    return ans
 
 
 print(threeSumClosest([-1, 2, 1, -4], 1))
