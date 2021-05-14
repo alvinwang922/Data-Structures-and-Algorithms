bda9105bbaffaf94ed00da9546113a93b5dff3de
Alvin Wang <alvinwang922@gmail.com>
2021-05-13 22:31:29 -0700

    Return the third maximum number in an array.

--- Arrays/Third-Max-Number.py
+++ Arrays/Third-Max-Number.py
@@ -6,26 +6,26 @@ complexity must be in O(n).
 """
 
 
-class Solution:
-    def thirdMax(self, nums: List[int]):
-        first, second, third = -float("inf"), \
-            -float("inf"), -float("inf")
-        for num in nums:
-            if num == first or num == second \
-                    or num == third:
-                continue
-            if num > first:
-                num, first = first, num
-            if num > second:
-                num, second = second, num
-            if num > third:
-                num, third = third, num
-        if third == -float("inf"):
-            return first
-        else:
-            return third
+def thirdMax(nums: List[int]):
+    first, second, third = -float("inf"), \
+        -float("inf"), -float("inf")
+    for num in nums:
+        if num == first or num == second \
+                or num == third:
+            continue
+        if num > first:
+            num, first = first, num
+        if num > second:
+            num, second = second, num
+        if num > third:
+            num, third = third, num
+    if third == -float("inf"):
+        return first
+    else:
+        return third
 
-    print(thirdMax([3, 2, 1]))
-    print(thirdMax([1, 2]))
-    print(thirdMax([3, 4, 5, 6, 7]))
-    print("The values above should be 1, 2, and 5.")
+
+print(thirdMax([3, 2, 1]))
+print(thirdMax([1, 2]))
+print(thirdMax([3, 4, 5, 6, 7]))
+print("The values above should be 1, 2, and 5.")
