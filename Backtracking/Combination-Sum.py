d0d05890860460cd1e944fc0133cc480f6d28ec2
Alvin Wang <alvinwang922@gmail.com>
2021-05-16 14:46:46 -0700

    Find all unique combinations of a list of integers that have the target sum.

--- Backtracking/Combination-Sum.py
+++ Backtracking/Combination-Sum.py
@@ -1,7 +1,7 @@
 """
-Given a set of candidate numbers (candidates) (without duplicates) 
+Given a set of candidate numbers (candidates) (without duplicates)
 and a target number (target), find all unique combinations in candidates
-where the candidate numbers sums to target. The same repeated number may 
+where the candidate numbers sums to target. The same repeated number may
 be chosen from candidates unlimited number of times.
 
 Note:
@@ -10,20 +10,20 @@ The solution set must not contain duplicate combinations
 """
 
 
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        self.backtrack(candidates, [], target, res)
-        return res
-    
-    def backtrack(self, candidates, combination, target, res):
-        if target == 0:
-            res.append(combination)
-        else:
-            for i in range(len(candidates)):
-                if target > 0:
-                    self.backtrack(candidates[i:], combination + [candidates[i]], \
-                    target - candidates[i], res)
+def combinationSum(candidates: List[int], target: int):
+    res = []
+    backtrack(candidates, [], target, res)
+    return res
+
+
+def backtrack(candidates, combination, target, res):
+    if target == 0:
+        res.append(combination)
+    else:
+        for i in range(len(candidates)):
+            if target > 0:
+                backtrack(candidates[i:], combination + [candidates[i]],
+                target - candidates[i], res)
 
 
 print(combinationSum([2, 3, 6, 7], 7))
