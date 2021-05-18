f28725ae766debf6639591982b8a09fdcaa26077
Alvin Wang <alvinwang922@gmail.com>
2021-05-18 14:28:07 -0700

    Generate all combinations for n set of parentheses.

--- Backtracking/Generate-Parentheses.py
+++ Backtracking/Generate-Parentheses.py
@@ -4,28 +4,27 @@ combinations of well-formed parentheses.
 """
 
 
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        result = []
-        self.backtrack(result, "", 0, 0, n * 2)
+def generateParenthesis(n):
+    """
+    :type n: int
+    :rtype: List[str]
+    """
+    result = []
+    backtrack(result, "", 0, 0, n * 2)
+    return result
+
+
+def backtrack(result, current, opening, closing, length):
+    if len(current) == length:
+        result.append(current)
         return result
+    if opening < length // 2:
+        backtrack(result, current + "(", opening + 1, closing, length)
+    if closing < opening:
+        backtrack(result, current + ")", opening, closing + 1, length)
 
-    def backtrack(self, result, current, opening, closing, length):
-        if len(current) == length:
-            result.append(current)
-            return result
-        if opening < length // 2:
-            self.backtrack(result, current + "(", opening + 1, closing, length)
-        if closing < opening:
-            self.backtrack(result, current + ")", opening, closing + 1, length)
-
-    def main():
-        print(generateParenthesis(1))
-        print(generateParenthesis(2))
-        print(generateParenthesis(3))
-        print("The arrays above should be ["()"], ["(())","()()"], and \
-            ["((()))", "(()())", "(())()", "()(())", "()()()"].")
+    print(generateParenthesis(1))
+    print(generateParenthesis(2))
+    print(generateParenthesis(3))
+    print("The arrays above should be ["()"], ["(())","()()"], and \
+        ["((()))", "(()())", "(())()", "()(())", "()()()"].")
