b313676022b5388d5c3fe1a3b6f48777418491a6
Alvin Wang <alvinwang922@gmail.com>
2021-05-21 16:34:56 -0700

    Determine all ways to put n queens on a chessboard such that the queens can't atttack each other.

--- Backtracking/N-Queens.py
+++ Backtracking/N-Queens.py
@@ -8,50 +8,51 @@ an empty space respectively.
 """
 
 
-class Solution:
-    def solveNQueens(self, n: int):
-        solutions = []
-        self.backtrack(n, solutions, [])
-        output = []
-        for solution in solutions:
-            board = []
-            for value in solution:
-                board_row = ""
-                for i in range(n):
-                    if value == i:
-                        board_row += 'Q'
-                    else:
-                        board_row += '.'
-                board.append(board_row)
-            output.append(board)
-        return output
+def solveNQueens(n: int):
+    solutions = []
+    backtrack(n, solutions, [])
+    output = []
+    for solution in solutions:
+        board = []
+        for value in solution:
+            board_row = ""
+            for i in range(n):
+                if value == i:
+                    board_row += 'Q'
+                else:
+                    board_row += '.'
+            board.append(board_row)
+        output.append(board)
+    return output
 
-    def backtrack(self, n, solutions, currentSolution):
-        if self.conflict(currentSolution) == True:
-            return
-        else:
-            if len(currentSolution) == n:
-                solutions.append(currentSolution[:])
-        for i in range(n):
-            currentSolution.append(i)
-            self.backtrack(n, solutions, currentSolution)
-            currentSolution.pop()
 
-    def conflict(self, solution):
-        if len(solution) == 1 or len(solution) == 0:
-            return False
-        else:
-            row = len(solution) - 1
-            col = solution[row]
+def backtrack(n, solutions, currentSolution):
+    if conflict(currentSolution) == True:
+        return
+    else:
+        if len(currentSolution) == n:
+            solutions.append(currentSolution[:])
+    for i in range(n):
+        currentSolution.append(i)
+        backtrack(n, solutions, currentSolution)
+        currentSolution.pop()
 
-            for i in range(len(solution) - 1):
-                if i == row or solution[i] == col or solution[i] + i \
+
+def conflict(solution):
+    if len(solution) == 1 or len(solution) == 0:
+        return False
+    else:
+        row = len(solution) - 1
+        col = solution[row]
+
+        for i in range(len(solution) - 1):
+            if i == row or solution[i] == col or solution[i] + i \
                     == col + row or solution[i] - i == col - row:
-                    return True
-            return False
+                return True
+        return False
 
 
 print(solveNQueens(4))
 print(solveNQueens(3))
 print("The lists above should be [[\".Q..\", \"...Q\", \"Q...\", \"..Q.\"],\
-    [\"..Q.\", \"Q...\", \"...Q\", \".Q..\"]]\" and [].)
+    [\"..Q.\", \"Q...\", \"...Q\", \".Q..\"]]\" and [].")
