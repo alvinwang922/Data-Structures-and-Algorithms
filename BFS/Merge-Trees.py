1268b2bfc6a9a83836d8440a2a459b8ba9b2053a
Alvin Wang <alvinwang922@gmail.com>
2021-05-25 14:42:19 -0700

    Given two binary trees, merge them.

--- BFS/Merge-Trees.py
+++ BFS/Merge-Trees.py
@@ -15,20 +15,19 @@ class TreeNode(object):
         self.right = right
 
 
-class Solution(object):
-    def mergeTrees(self, t1, t2):
-        """
-        :type t1: TreeNode
-        :type t2: TreeNode
-        :rtype: TreeNode
-        """
-        if t1 and t2:
-            t1.val += t2.val
-            t1.left = self.mergeTrees(t1.left, t2.left)
-            t1.right = self.mergeTrees(t1.right, t2.right)
-            return t1
-        else:
-            return t1 or t2
+def mergeTrees(self, t1, t2):
+    """
+    :type t1: TreeNode
+    :type t2: TreeNode
+    :rtype: TreeNode
+    """
+    if t1 and t2:
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+    else:
+        return t1 or t2
 
 
 print(mergeTrees([1, 3, 2, 5], [2, 1, 3, null, 4, null, 7]))
